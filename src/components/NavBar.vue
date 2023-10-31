0161113104b8fc8915181a15020db949478faf13
N-011 <noah171202@gmail.com>
2023-11-01 02:55:43 +0400

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar-container">
+    <RouterLink to="/" class="navbar-item" id="nav-logo">
+      <h1 id="nk">NK</h1>
+      <h1 id="mt">MT</h1>
+    </RouterLink>
+    <RouterLink to="/about" class="navbar-item">
+      <h1>ABOUT</h1>
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style></style>
+
+<!-- <script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script> -->
