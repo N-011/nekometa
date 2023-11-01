9fec26fb14878a4c90df040fc83dcbaf7b742321
N-011 <noah171202@gmail.com>
2023-11-01 16:33:50 +0400

    feat: added loader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
+  <div class="loader"></div>
   <NavBar />
   <router-view />
 </template>
 
 <script setup>
 import NavBar from "./components/NavBar.vue";
+window.addEventListener("load", () => {
+  document.querySelector(".loader").classList.add("loader--hidden");
+});
 </script>
 
 <style>
