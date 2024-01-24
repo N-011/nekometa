c77e27f53d398a7b995243e3126dee6c23a96dcf
N-011 <noah171202@gmail.com>
2024-01-24 15:55:26 +0400

    added dist subtree

---
+++ dist/js/about.04116a95.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunknekometa"]=self["webpackChunknekometa"]||[]).push([[443],{833:function(n,e,t){t.r(e),t.d(e,{default:function(){return k}});var a=t(3396);const u={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,e){return(0,a.wg)(),(0,a.iD)("div",u,s)}var o=t(89);const i={},f=(0,o.Z)(i,[["render",c]]);var k=f}}]);
+//# sourceMappingURL=about.04116a95.js.map
